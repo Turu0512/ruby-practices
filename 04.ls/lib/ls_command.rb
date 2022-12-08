ba7203a40ba1be7cf58af4201096eef1244bfd63
つるつる <hinataptyan@gmail.com>
2022-12-08 14:54:53 +0900

    リストを分割するメソッドの作成

--- 04.ls/lib/ls_command.rb
+++ 04.ls/lib/ls_command.rb
@@ -1,25 +1,43 @@
 class LsCommand
   def initialize()
-      @current_directory = Dir.getwd
+    @current_directory = Dir.getwd
+    @files = Array.new
   end
   
-  def self.run
-    ls = new
-    ls.check_current_directory
+  def run
+    files = get_list
+    screen_list = list_to_show(files)
   end
 
-  def check_current_directory
-    puts @current_directory
-    screen_in_the_directory
-  end
-
-  def screen_in_the_directory
+  def get_list
     Dir.foreach(@current_directory) do |item|
       next if item == '.' or item == '..'
-      puts item
+      @files.push(item)
+    end
+    @files.sort!
+  end
+
+  def list_to_show(files)
+    # 列の数を指定する。
+    columuns = 3
+    max_rows = if files.size % columuns == 0
+      files.size / columuns
+    else
+        (files.size / columuns) + 1
+    end
+
+    split_files = columuns.times.map do |i|
+      if i.zero?
+        files.slice(0, max_rows)
+      else
+        files.slice((max_rows) * i,max_rows)
+      end
+    end
+  end
+
     end
   end
 
 end
 
-LsCommand.run
+LsCommand.new.run
