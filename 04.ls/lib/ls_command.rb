88f563b62f240a19bed68bc3e27bd9cb754bb133
つるつる <hinataptyan@gmail.com>
2022-12-08 14:57:49 +0900

    分割したリストを表示形式に変換して表示する

--- 04.ls/lib/ls_command.rb
+++ 04.ls/lib/ls_command.rb
@@ -7,6 +7,7 @@ class LsCommand
   def run
     files = get_list
     screen_list = list_to_show(files)
+    screen_in_the_directory(screen_list)
   end
 
   def get_list
@@ -35,7 +36,22 @@ class LsCommand
     end
   end
 
+  def screen_in_the_directory(list)
+    # 一番長い文字列のsizeを取得
+    max_file_name_len = 1
+    @files.each do |file|
+      if file.size >= max_file_name_len
+        max_file_name_len = file.size
+      end
     end
+
+    output_files_list = (0..list[0].size).map do |i|
+      output_file = list.map do |file_array|
+        file_array[i]&.ljust(max_file_name_len + 3)
+      end
+      output_file.join
+    end
+    puts output_files_list
   end
 
 end
