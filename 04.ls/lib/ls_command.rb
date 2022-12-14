a14d3a533d77fae45d60e9ec8adea464f4cbd21b
つるつる <hinataptyan@gmail.com>
2022-12-14 18:19:07 +0900

    transposeで並び替えるように変更と、メソッドの切り分け

--- 04.ls/lib/ls_command.rb
+++ 04.ls/lib/ls_command.rb
@@ -9,7 +9,7 @@ class LsCommand
   def run
     files = make_list
     screen_list = list_to_show(files)
-    screen_in_the_directory(screen_list)
+    screen_in_the_directory(screen_list, files)
   end
 
   def make_list
@@ -23,29 +23,45 @@ class LsCommand
   end
 
   def list_to_show(files)
-    # 列の数を指定する。
-    max_rows, mod = files.size.divmod(COLUMN)
-    max_rows += 1 unless mod.zero?
-
+    max_rows = max_rows(files)
     Array.new(COLUMN) do |i|
       files.slice(max_rows * i, max_rows)
     end
   end
 
-  def screen_in_the_directory(list)
+  def screen_in_the_directory(list, files)
     # 一番長い文字列のsizeを取得
-    max_file_name_len = 1
-    list.each do |files|
-      max_file_name_len = files.max_by(&:size).size if files.max_by(&:size).size >= max_file_name_len
-    end
-    output_files_list = (0..list[0].size).map do |i|
-      output_file = list.map do |file_array|
-        file_array[i]&.ljust(max_file_name_len + 3)
-      end
-      output_file.join
+    spacing_between_files = spacing_between_files(list)
+    max_rows = max_rows(files)
+    # 配列の長さを揃えて、transposeする
+    align_list_length = list.each do |item|
+      item.size < max_rows ? item.push(nil) : item
+    end.transpose
+    # 空白を追加してjoinする
+    output_files_list = align_list_length.map do |array|
+      array.map do |file1|
+        file1&.ljust(spacing_between_files)
+      end.join
     end
     puts output_files_list
   end
+
+  def max_rows(files)
+    # 列の数を指定する
+    max_rows, mod = files.size.divmod(COLUMN)
+    max_rows += 1 unless mod.zero?
+    max_rows
+  end
+
+  def spacing_between_files(list)
+    # 出力したファイル名の間隔を決定する
+    spacing_between_files = 1
+    list.each do |item|
+      spacing_between_files = item.max_by(&:size).size + 3 if item.max_by(&:size).size >= max_file_name_len
+    end
+    spacing_between_files
+  end
+
 end
 
 LsCommand.new.run
